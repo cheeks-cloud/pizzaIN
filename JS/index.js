b7ac4bf07449247dfefe8469cd62414357c018e3
@cheeks cloud <agneskaswii700@gmail.com>
2022-03-20 22:31:24 +0300

    Adding initial Commits

---
+++ JS/index.js
@@ -0,0 +1,105 @@
+// //pizza constructor
+// function Pizza(pizzaSize, crustType, topping,numberof, delivery){
+//   this.pizzaSize = pizzaSize
+//   this.crustType = crustType
+//   this.topping = topping
+//   this.numberof = numberof
+//   this.delivery = delivery
+
+//  }
+// // function to calaculate total price
+// //pizza prototype
+//  let totalOfTotals = 0;
+
+//   Pizza.prototype.calculatePrice = function(){
+
+//     let deliveryprize;
+//     let pizzaPrice;
+//     let crustPrize ;
+//     let toppingPrize;
+//     let numberofPizza;
+    
+//    if(this.pizzaSize ==="small"){
+//      pizzaPrice = 350
+//    }else if(this.pizzaSize ==="medium"){
+//      pizzaPrice = 600
+//    }else{
+//      pizzaPrice = 800
+//    }
+   
+//     if(this.crustType  === "crispy"){
+//       crustPrize = 150
+//     }else if(this.crustType === "stuffed"){
+//       crustPrize = 100
+//     }else{
+//       crustPrize = 70
+//     }
+   
+//     if(this.topping ==="Roasted Tomatoes"){
+//       toppingPrize = 30
+//     }else if(this.topping ==="Roasted Artichokes")
+//     {toppingPrize =35  
+  
+//     }else{
+//       toppingPrize = 40
+//     }
+//    if(this.numberof <=0 ){
+//      numberofPizza = 0
+//    }else{
+//      numberofPizza = this.numberof
+//    }
+//    if(this.delivery === "Yes"){
+//      deliveryprize = 100
+//    }else{
+//      deliveryprize = 0
+//    }
+   
+//     let totalPrice = (pizzaPrice + crustPrize + toppingPrize) * numberofPizza
+//     let total = totalPrice + deliveryprize
+
+//     totalOfTotals += total
+//    return total
+//   }
+
+// $(document).ready(function(){
+//   $("button#order").click(function(e){
+//     e.preventDefault()
+//     let pizza =$("#pizzaSize").find("option:selected").val()
+//     let crust =$("#crustType").find("option:selected").val()
+//     let topping=$("#topping").find("option:selected").val()
+//     let pizzaNumber = $("#noOfPizza").val()
+//     let delivery = $("input[name = 'Yes']:checked").val()
+//     let newOrder = new Pizza(pizza, crust, topping , pizzaNumber, delivery)
+
+//    $("#orders").append(
+//     "<tr>"+ "<td>" + pizza +"</td>" +  "<td>"+ crust+"</td>" + 
+//    " <td>"+ topping +"</td>" + "<td>"+ pizzaNumber + "</td>" +
+//    "<td>"+ newOrder.calculatePrice() + "</td>" + 
+//     "</tr>"  
+//    )
+  
+//   })
+// });
+// $("#yes").click(function(){
+//   $(".addons").show()
+// })
+// $("#no").click(function(){
+//   $(".addons").hide()
+// })
+// $("#checkout").click(function(){
+//  console.log(totalOfTotals)
+//  $(".last").text(`The Total of Your Order is : ${ totalOfTotals}`)
+
+// })
+
+
+
+
+
+// //carousel js
+// document.addEventListener("DOMContentLoaded", function(){
+//   var element = document.getElementById("myCarousel");
+//   var myCarousel = new bootstrap.Carousel(element, {
+//       interval: 2000
+//   });
+// });
---
+++ README.md
@@ -0,0 +1,72 @@
+# Cheecks Pizza
+
+### version vi.o.3
+
+# By Agnes Kaswii
+## Table of contents
+
++ [Description](#description)
++ [Installation Requirements](#installation)
++ [Technology used](#technology-used)
++ [Reference](#reference)
++ [Licence](#license)
++ [Authors Info](#author-Info)
++ [Contact Me](#contact-me)
++ [BDD](#bdd)
+
+## Description
+<p>This project is a Pizza website,it shows a alanding page and  a place to make orders that has full functionality</p>
+
+## Installation
+
+### Requirements
+* Access to the internet
+* A computer,tablet,phone,Ipad or laptop
+
+### Installation Process
+
+## setup/installation instructions
+* you need to link your index.html to bootstrap.
+* you also need to link your js file to the index.html and order.html
+
+ [Go back to the top]( #)
+
+ ## Technology Used
+ * HTML which was used to build the structure of the page.
+ * CSS  which was used to style the page.
+ * media queries to make it responsive.
+ * bootsrap to help in styling
+ * javascript to give it functionality.
+ * jQuery to add functionality and Effects
+
+ ## Reference
+  * Materialise CSS.
+  * bootstrap starter template.
+  * MDN javascript material.
+  * MDN javascript material.
+
+  [Go Back to the top]( #)
+
+  ## License
+   copyright (c) 2022 License [click to view License](lICENSE)
+
+   [Go back to the top](#)
+
+   ## Authors Info
+
+   Slack profile - [Agnes Kaswii]
+
+  ( https://app.slack.com/client/T0101L740P4/C02TTF7MR8D/user_profile/U02UL5C9NE4)
+
+  Linked - [Agnes Kaswii]
+  (https://www.linkedin.com/in/agnes-kaswii-6442b91bb/)
+
+    ## Contact Me
+
+  Email - [Agnes Kaswii]
+  agneskaswii700@gmail.com
+
+  [Go back to the top](#)
+
+  ## BDD
+  
